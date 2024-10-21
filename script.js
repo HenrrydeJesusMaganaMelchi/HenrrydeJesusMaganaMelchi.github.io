8792d2aaf4cd0d8479953729fa2dfb4092f0ca21
HenrrydeJesusMaganaMelchi <henrrymelchi@gmail.com>
2024-10-20 21:21:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Seleccionamos el botón por su ID
+document.getElementById("myButton1").onclick = function() {
+    // Redirige a la página especificada
+    window.location.href = "Practicas/Practica 1.html";
+  };
+document.getElementById("myButton2").onclick = function() {
+    // Redirige a la página especificada
+    window.location.href = "Practicas/Practica_2.html";
+  };
+document.getElementById("myButton3").onclick = function() {
+    // Redirige a la página especificada
+    window.location.href = "Practicas/Practica 3.html";
+  };
+document.getElementById("myButton4").onclick = function() {
+    // Redirige a la página especificada
+    window.location.href = "Practicas/Practica 4.html";
+  };
