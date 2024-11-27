e81adefc1c88421035c0898283ee0adf819e7f8a
HenrrydeJesusMaganaMelchi <henrrymelchi@gmail.com>
2024-11-26 20:15:53 -0500

    correccion de practica 10

--- php/Practica10_php.php
+++ php/Practica10_php.php
@@ -1,4 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 10</title>
+</head>
+<body>
+    <h1>Practica 10: Listar bases de datos</h1><br>
+    <img width="1000px" src="../img/Pract10.png" alt="">
 <?php
+/*
 require("config.php");
 
 echo "<h1>Práctica 10: Listar bases de datos</h1><br><br>";
@@ -21,5 +32,7 @@ try {
     echo "</table><br><br>";
 } catch (PDOException $e) {
     echo "Error al listar las bases de datos: " . $e->getMessage();
-}
+}*/
 ?>
+</body>
+</html>
